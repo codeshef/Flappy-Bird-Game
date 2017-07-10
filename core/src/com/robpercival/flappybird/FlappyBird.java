82defd25ddfaa9f7088be1ceca6082897b64c8d7
codeshef <mukulanchal@gmail.com>
2017-07-10 10:15:51 +0530

    Working with sprites.

---
+++ core/src/com/robpercival/flappybird/FlappyBird.java
@@ -0,0 +1,33 @@
+                                                                                                                                                                                                                              package com.robpercival.flappybird;
+
+import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+
+public class FlappyBird extends ApplicationAdapter {
+	SpriteBatch batch;
+	Texture background;
+    Texture bird;
+	
+	@Override
+	public void create () {
+		batch = new SpriteBatch();
+		// selecting background image
+		background =new Texture("bg.png");
+        bird = new Texture("bird.png");
+
+	}
+
+	@Override
+	public void render () {
+
+		// tells that we begin using sprites now
+		batch.begin();
+		batch.draw(background,0,0,Gdx.graphics.getWidth(),Gdx.graphics.getHeight());
+        batch.draw(bird,Gdx.graphics.getWidth()/2-bird.getWidth()/2,Gdx.graphics.getHeight()/2 -bird.getHeight()/2);
+		batch.end();
+
+	}
+}
