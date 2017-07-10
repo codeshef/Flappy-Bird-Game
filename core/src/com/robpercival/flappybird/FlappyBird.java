c0cb25d4482c4dfaf87ff63484c93f1502968936
codeshef <mukulanchal@gmail.com>
2017-07-10 18:09:28 +0530

    Adding top and bottom tubes.

--- core/src/com/robpercival/flappybird/FlappyBird.java
+++ core/src/com/robpercival/flappybird/FlappyBird.java
@@ -1,31 +1,67 @@
-                                                                                                                                                                                                                              package com.robpercival.flappybird;
+package com.robpercival.flappybird;
 
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.g3d.particles.influencers.ColorInfluencer;
+
+import java.util.Random;
 
 public class FlappyBird extends ApplicationAdapter {
-	SpriteBatch batch;
-	Texture background;
+    SpriteBatch batch;
+    Texture background;
     Texture[] birds;
-	int flapState =0;
+
+    int flapState =0;
     float birdY =0;
     float velocity =0;
 
     int gameState =0;
     float gravity = 2;
+
+    Texture topTube;
+    Texture bottomTube;
+
+    Random getRandomGenerator;
+
+    float tubeVelocity =4;
+    int  numberOfTubes =4;
+    float[] tubeX = new float[numberOfTubes];
+    float[] tubeOffset = new float[numberOfTubes];
+    float distanceBetweenTubes;
+
+
+                                                                                                                                                                                                                                  float gap = 400;float maxTubeOffSet;Random randomGenerator;
 	
 	@Override
 	public void create () {
 		batch = new SpriteBatch();
 		// selecting background image
-		background =new Texture("bg.png");
+
+        background =new Texture("bg.png");
+
         birds= new Texture[2];
         birds[0]= new Texture("bird.png");
 		birds[1]= new Texture("bird2.png");
         birdY = Gdx.graphics.getHeight()/2 -birds[0].getHeight()/2;
+
+        topTube =   new Texture("toptube.png");
+        bottomTube = new Texture("bottomtube.png");
+
+        maxTubeOffSet = Gdx.graphics.getHeight()/2 - gap/2 -100;
+
+        randomGenerator = new Random();
+        distanceBetweenTubes = Gdx.graphics.getWidth()*3/4;
+
+        for(int i=0;i<numberOfTubes;i++){
+
+            tubeOffset[i]= (randomGenerator.nextFloat()-0.5f)*(Gdx.graphics.getHeight()- gap-200);
+
+            tubeX[i] = Gdx.graphics.getWidth()/2 - topTube.getWidth()/2 + i*distanceBetweenTubes;
+
+        }
 
 
 	}
@@ -34,17 +70,36 @@ public class FlappyBird extends ApplicationAdapter {
 	public void render () {
 
 
+        batch.begin();
+        batch.draw(background, 0, 0, Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
+
+        if(gameState!=0) {
 
 
-         if(gameState!=0) {
+            if(Gdx.input.justTouched()){
+
+                velocity=-30;
 
 
+            }
 
-             if(Gdx.input.justTouched()){
+            for(int i=0;i<numberOfTubes;i++) {
 
-                 velocity=-30;
 
-             }
+                if (tubeX[i] < topTube.getWidth()) {
+
+                    tubeX[i] += numberOfTubes * distanceBetweenTubes;
+                } else {
+
+                    tubeX[i] = tubeX[i] - tubeVelocity;
+                }
+
+                    batch.draw(topTube, tubeX[i], Gdx.graphics.getHeight() / 2 + gap / 2 + tubeOffset[i]);
+
+                    batch.draw(bottomTube, tubeX[i], Gdx.graphics.getHeight() / 2 - gap / 2 - bottomTube.getHeight() + tubeOffset[i]);
+
+
+            }
 
              if(birdY > 0 || velocity < 0) {
                  velocity = velocity+gravity;
@@ -71,8 +126,7 @@ public class FlappyBird extends ApplicationAdapter {
 
         }
 
-        batch.begin();
-        batch.draw(background, 0, 0, Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
+
         batch.draw(birds[flapState], Gdx.graphics.getWidth() / 2 - birds[flapState].getWidth() / 2, birdY);
         batch.end();
 
